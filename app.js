2d3ee707ba09ed3b3d1badb1ec9d55d29f459888
sbaidon <sbaidon@outlook.com>
2015-08-31 17:31:43 -0600

    changed port number

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var client = new opcua.OPCUAClient();
 
 var hostname = require("os").hostname();
 hostname = hostname.toLowerCase();
-var endpointUrl = "opc.tcp://10.33.27.0:49320";
+var endpointUrl = "opc.tcp://10.33.27.0";
 
 var the_subscription,the_session;
 
